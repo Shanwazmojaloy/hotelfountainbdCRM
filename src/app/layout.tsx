2d551b6230503a99dd216a5b873383ce3786917d
Shanwazmojaloy <ahmedshanwaz5@gmail.com>
2026-04-23 09:08:27 +0000

    feat: wire useCheckout to BillingCard + add QueryProvider to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,14 @@
 
 import "./globals.css";
+import { QueryProvider } from "@/providers/QueryProvider";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="h-full antialiased font-sans">
       <body className="min-h-full flex flex-col antialiased" style={{ background: '#07090E' }}>
-        {children}
+        <QueryProvider>
+          {children}
+        </QueryProvider>
       </body>
     </html>
   );
