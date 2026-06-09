712d98c35f7d31f9bc99558155e570783d671621
Shan <ahmedshanwaz5@gmail.com>
2026-06-09 11:06:47 +0600

    feat(crm): port Room Matrix display to /crm/rooms (monolith->app migration, screen 4/7)

---
+++ app/crm/rooms/page.tsx
@@ -0,0 +1,10 @@
+import Layout from '@/components/Layout';
+import Rooms from '@/components/Rooms';
+
+export default function RoomsRoute() {
+  return (
+    <Layout>
+      <Rooms />
+    </Layout>
+  );
+}
